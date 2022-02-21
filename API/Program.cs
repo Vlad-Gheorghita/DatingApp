97217918274dccdbf33af4376b3bf7c514983507
Vlad <vlad.gheorghita.s@outlook.com>
2022-02-21 20:54:00 +0200

    Deploy to heroku

--- API/Program.cs
+++ API/Program.cs
@@ -29,7 +29,43 @@ builder.Services.AddScoped<LogUserActivity>();
 builder.Services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
 builder.Services.AddDbContext<DataContext>(options =>
 {
-    options.UseSqlite(connectionString);
+    //options.UseSqlite(connectionString);
+    //options.UseNpgsql(connectionString);
+
+    
+    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+    string connStr;
+
+    // Depending on if in development or production, use either Heroku-provided
+    // connection string, or development connection string from env var.
+    if (env == "Development")
+    {
+        // Use connection string from file.
+        connStr = builder.Configuration.GetConnectionString("DefaultConnection");
+    }
+    else
+    {
+        // Use connection string provided at runtime by Heroku.
+        var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+        // Parse connection URL to connection string for Npgsql
+        connUrl = connUrl.Replace("postgres://", string.Empty);
+        var pgUserPass = connUrl.Split("@")[0];
+        var pgHostPortDb = connUrl.Split("@")[1];
+        var pgHostPort = pgHostPortDb.Split("/")[0];
+        var pgDb = pgHostPortDb.Split("/")[1];
+        var pgUser = pgUserPass.Split(":")[0];
+        var pgPass = pgUserPass.Split(":")[1];
+        var pgHost = pgHostPort.Split(":")[0];
+        var pgPort = pgHostPort.Split(":")[1];
+
+        connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Require;TrustServerCertificate=True";
+    }
+
+    // Whether the connection string came from the local development configuration file
+    // or from the environment variable from Heroku, use it to set up your DbContext.
+    options.UseNpgsql(connStr);
 });
 
 builder.Services.AddControllers();
@@ -88,6 +124,8 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
+
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);    //asta e de la PostgreSQL 
 
 try
 {
